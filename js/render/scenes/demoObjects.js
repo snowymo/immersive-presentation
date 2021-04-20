af43c3e4760a5d626f70e267fc6bb36e1076eed1
KeruWang <kw2727@nyu.edu>
2021-04-20 18:23:58 -0400

    moved back circle text

--- js/render/scenes/demoObjects.js
+++ js/render/scenes/demoObjects.js
@@ -13,33 +13,60 @@ let DemoObjects = function () {
     m.save();
     m.translate(0, 1, -0.4);
     m.scale(0.04);
-    renderList.mCube().move(-4.5, 4.5, 0).turnY(time).color([1, 1, 1]).setBaseTexture("brick.png");
-    renderList.mQuad().move(-1.5, 4.5, 0).turnY(time).color([1, 1, 1]).setBaseTexture("stones.jpg");;
-    renderList.mSquare().move(1.5, 4.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mSphere().move(4.5, 4.5, 0).turnY(time).color([1, 1, 1]);
-    // renderList.mCylinder().move(-4.5, 1.5, 0).turnY(time).color([1, 1, 1]).textureView(window.textures[3].lookupImageByID(1)).textureAtlas(window.textures[3]);
-    renderList
-      .mRoundedCylinder()
-      .move(-1.5, 1.5, 0)
-      .turnY(time)
-      .color([1, 1, 1]);
-    renderList.mTorus().move(1.5, 1.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mDisk().move(4.5, 1.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mCone().move(-4.5, -1.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mTube().move(-1.5, -1.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mTube3().move(1.5, -1.5, 0).turnY(time).color([1, 1, 1]);
-    renderList.mGluedCylinder().move(4.5, -1.5, 0).turnY(time).color([1, 1, 1]);
+
+    renderList.mCube().move(-4.5, 4.5, 0)
+                      .turnY(time).color([1, 1, 1])
+                      .setBaseTexture("brick.png");
+    renderList.mQuad().move(-1.5, 4.5, 0)
+                      .turnY(time).color([1, 1, 1])
+                      .setBaseTexture("stones.jpg");;
+    renderList.mSquare().move(1.5, 4.5, 0)
+                        .turnY(time).color([1, 1, 1]);
+    renderList.mSphere().move(4.5, 4.5, 0)
+                        .turnY(time).color([1, 1, 1]);
+    renderList.mCylinder().move(-4.5, 1.5, 0)
+                          .turnY(time).color([1, 1, 1])
+			  .textureView(window.textures[1].lookupImageByID(1))
+			  .textureAtlas(window.textures[1]);
+    renderList.mRoundedCylinder().move(-1.5, 1.5, 0)
+                                 .turnY(time)
+                                 .color([1, 1, 1])
+				 .textureView(window.textures[2].lookupImageByID(1))
+				 .textureAtlas(window.textures[2]);
+    renderList.mTorus().move(1.5, 1.5, 0)
+                       .turnY(time).color([1, 1, 1]);
+    renderList.mDisk().move(4.5, 1.5, 0)
+                      .turnY(time).color([1, 1, 1]);
+    renderList.mCone().move(-4.5, -1.5, 0)
+                      .turnY(time).color([1, 1, 1]);
+    renderList.mTube().move(-1.5, -1.5, 0)
+                      .turnY(time).color([1, 1, 1]);
+    renderList.mTube3().move(1.5, -1.5, 0)
+                      .turnY(time).color([1, 1, 1]);
+    renderList.mGluedCylinder().move(4.5, -1.5, 0)
+                               .turnY(time)
+			       .color([1, 1, 1]);
+
+    let isCircle = time % 4 > 2;
     m.save();
-       CG.particlesTextMessage(P, '  These are\nsome Metaroom\n   objects');
+       let msg = '  These are\nsome Metaroom\n   objects', A;
+
+       if (isCircle) {
+          A = [];
+          for (let n = 0 ; n < msg.length ; n++) {
+             let theta = Math.PI - 2 * Math.PI * n / msg.length;
+             A.push([10 * Math.cos(theta), 5 * Math.sin(theta), 0]);
+          }
+       }
+       CG.particlesTextMessage(P, msg, A);
        m.translate(0,0,3);
        m.rotateY(Math.sin(2 * time));
        m.scale(.3,.6,.6);
-       m.translate(-5,4,0);
+       m.translate(isCircle ? 1 : -5,isCircle ? 3 : 4,0);
        renderList.mMesh(P).color([10, 10, 0])
                           .textureView(window.textures[4].lookupImageByID(1))
 		          .textureAtlas(window.textures[4]);
     m.restore();
-
 
     m.restore();
   };
