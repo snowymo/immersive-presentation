d237e4f9ef935476e762e258a4aa3d7f48a248fa
Zhenyi He <snowymohehe@gmail.com>
2021-05-05 23:59:11 +0800

    :bug: fix media relative path

--- js/render/scenes/demoAirText.js
+++ js/render/scenes/demoAirText.js
@@ -8,41 +8,41 @@ import { airfont } from "../core/airfont.js";
 let msg = 'Future Reality Lab\n    Metaroom', P;
 {
    let total = 0;
-   for (let i = 0 ; i < msg.length ; i++)
-      total += airfont.strokeLength(msg.substring(i, i+1));
+   for (let i = 0; i < msg.length; i++)
+      total += airfont.strokeLength(msg.substring(i, i + 1));
    P = CG.particlesCreateMesh(Math.floor(70 * total));
 }
 
 let DemoAirText = function () {
-   this.background = "../../media/gltf/cave/cave.gltf";
+   this.background = "./media/gltf/cave/cave.gltf";
    this.loadGLTF = false;
    this.envInd = null;
 
    this.display = () => {
-      let mix = (a,b,t) => a + t * (b - a);
+      let mix = (a, b, t) => a + t * (b - a);
       let R = [], x = 0, y = 3;
       let t = .5 + .5 * Math.cos(time / 3);
       CG.random(0);
-      for (let i = 0 ; i < msg.length ; i++) {
-         let ch = msg.substring(i, i+1);
+      for (let i = 0; i < msg.length; i++) {
+         let ch = msg.substring(i, i + 1);
          if (ch == '`') {
-            let j = msg.indexOf('`', i+1);
+            let j = msg.indexOf('`', i + 1);
             if (j >= 0) {
-               if (j > i+1)
-                  ch = msg.substring(i+1, j);
+               if (j > i + 1)
+                  ch = msg.substring(i + 1, j);
                i = j;
             }
          }
          let ns = 50 * airfont.strokeLength(ch);
-         for (let n = 0 ; n < ns ; n++) {
+         for (let n = 0; n < ns; n++) {
             let xy = airfont.eval(n / ns, ch);
             let rx = CG.random(), ry = CG.random(), rz = CG.random();
-            rx += .3 * Math.cos(1000 * n/ns + time/2 * (n%2 ? 1 : -1));
-            rz += .3 * Math.sin(1000 * n/ns + time/2 * (n%2 ? 1 : -1));
+            rx += .3 * Math.cos(1000 * n / ns + time / 2 * (n % 2 ? 1 : -1));
+            rz += .3 * Math.sin(1000 * n / ns + time / 2 * (n % 2 ? 1 : -1));
             R.push([mix(0.5 * x + xy[0], 1 + 6 * rx, t),
-                    mix(1.5 * y + xy[1], 2.4 + 1.5 * ry, t),
-                    mix(0, 6 * rz - 1.5, t), .03,
-                    i/msg.length, CG.random(), 1 - i/msg.length]);
+            mix(1.5 * y + xy[1], 2.4 + 1.5 * ry, t),
+            mix(0, 6 * rz - 1.5, t), .03,
+            i / msg.length, CG.random(), 1 - i / msg.length]);
          }
          x++;
          if (ch == '\n') {
@@ -52,8 +52,8 @@ let DemoAirText = function () {
       }
       CG.particlesSetPositions(P, R, CG.matrixMultiply(viewMatrix[0], m.value()));
       m.save();
-         m.translate(-.35,1.,-.5);
-         renderList.mMesh(P).size(.1).color([10,10,10]);
+      m.translate(-.35, 1., -.5);
+      renderList.mMesh(P).size(.1).color([10, 10, 10]);
       m.restore();
    }
 }
--- js/render/scenes/demoObjects.js
+++ js/render/scenes/demoObjects.js
@@ -3,75 +3,75 @@ import { CG } from "../core/CG.js";
 import { time } from "../core/renderListScene.js";
 
 let DemoObjects = function () {
-  this.background = "../../media/gltf/home-theater/home-theater.gltf";
-  this.loadGLTF = false;
-  this.envInd = null;
+   this.background = "./media/gltf/home-theater/home-theater.gltf";
+   this.loadGLTF = false;
+   this.envInd = null;
 
-  this.display = () => {
-    m.save();
-       m.translate(0, 0, -0.4);
-       m.scale(0.04);
+   this.display = () => {
+      m.save();
+      m.translate(0, 0, -0.4);
+      m.scale(0.04);
 
-       renderList.mCube().move(-4.5, 4.5, 0)
-                         .turnY(time).color([1, 1, 1])
-                         .setBaseTexture("brick.png");
-       renderList.mQuad().move(-1.5, 4.5, 0)
-                         .turnY(time).color([1, 1, 1])
-                         .setBaseTexture("stones.jpg");;
-       renderList.mSquare().move(1.5, 4.5, 0)
-                           .turnY(time).color([10, 10, 10])
-                           .setBaseTexture("font.png");
-       renderList.mSphere().move(4.5, 4.5, 0)
-                           .turnY(time).color([1, 1, 1]);
+      renderList.mCube().move(-4.5, 4.5, 0)
+         .turnY(time).color([1, 1, 1])
+         .setBaseTexture("brick.png");
+      renderList.mQuad().move(-1.5, 4.5, 0)
+         .turnY(time).color([1, 1, 1])
+         .setBaseTexture("stones.jpg");;
+      renderList.mSquare().move(1.5, 4.5, 0)
+         .turnY(time).color([10, 10, 10])
+         .setBaseTexture("font.png");
+      renderList.mSphere().move(4.5, 4.5, 0)
+         .turnY(time).color([1, 1, 1]);
 
-       renderList.mCylinder().move(-4.5, 1.5, 0)
-                             .turnY(time).color([1, 1, 1])
-                             .textureView(window.textures[1].lookupImageByID(1))
-                             .textureAtlas(window.textures[1]);
-       renderList.mRoundedCylinder().move(-1.5, 1.5, 0)
-                                    .turnY(time)
-                                    .color([1, 1, 1])
-                                    .textureView(window.textures[2].lookupImageByID(1))
-                                    .textureAtlas(window.textures[2]);
-       renderList.mTorus().move(1.5, 1.5, 0)
-                          .turnY(time).color([1, 1, 1]);
-       renderList.mDisk().move(4.5, 1.5, 0)
-                         .turnY(time).color([1, 1, 1]);
+      renderList.mCylinder().move(-4.5, 1.5, 0)
+         .turnY(time).color([1, 1, 1])
+         .textureView(window.textures[1].lookupImageByID(1))
+         .textureAtlas(window.textures[1]);
+      renderList.mRoundedCylinder().move(-1.5, 1.5, 0)
+         .turnY(time)
+         .color([1, 1, 1])
+         .textureView(window.textures[2].lookupImageByID(1))
+         .textureAtlas(window.textures[2]);
+      renderList.mTorus().move(1.5, 1.5, 0)
+         .turnY(time).color([1, 1, 1]);
+      renderList.mDisk().move(4.5, 1.5, 0)
+         .turnY(time).color([1, 1, 1]);
 
-       renderList.mCone().move(-4.5, -1.5, 0)
-                         .turnY(time).color([1, 1, 1]);
-       renderList.mTube().move(-1.5, -1.5, 0)
-                         .turnY(time).color([1, 1, 1]);
-       renderList.mTube3().move(1.5, -1.5, 0)
-                          .turnY(time).color([1, 1, 1]);
-       renderList.mGluedCylinder().move(4.5, -1.5, 0)
-                                  .turnY(time)
-                                  .color([1, 1, 1]);
+      renderList.mCone().move(-4.5, -1.5, 0)
+         .turnY(time).color([1, 1, 1]);
+      renderList.mTube().move(-1.5, -1.5, 0)
+         .turnY(time).color([1, 1, 1]);
+      renderList.mTube3().move(1.5, -1.5, 0)
+         .turnY(time).color([1, 1, 1]);
+      renderList.mGluedCylinder().move(4.5, -1.5, 0)
+         .turnY(time)
+         .color([1, 1, 1]);
 
-       m.save();
-          m.translate(0,0,3);
-          m.rotateY(Math.sin(2*time));
-          m.translate(-2,1,0);
-          m.scale(.3,.6,.6);
+      m.save();
+      m.translate(0, 0, 3);
+      m.rotateY(Math.sin(2 * time));
+      m.translate(-2, 1, 0);
+      m.scale(.3, .6, .6);
 
-          let t = Math.max(0, Math.min(1, .5 + Math.sin(time)));
-          for (let n = 0 ; n < msg.length ; n++)
-             C[n] = CG.mix(A[n], B[n], t);
+      let t = Math.max(0, Math.min(1, .5 + Math.sin(time)));
+      for (let n = 0; n < msg.length; n++)
+         C[n] = CG.mix(A[n], B[n], t);
 
-          PT.layout(C);
-          renderList.mMesh(PT.mesh()).color([10, 10, 0])
-                                     .setBaseTexture('font.png');
-       m.restore();
+      PT.layout(C);
+      renderList.mMesh(PT.mesh()).color([10, 10, 0])
+         .setBaseTexture('font.png');
+      m.restore();
 
-    m.restore();
-  };
+      m.restore();
+   };
 };
 
 let msg = '  These are\nsome Metaroom\n   objects';
 let PT = new CG.ParticlesText(msg);
 
 let A = PT.layout(), B = [], C = [];
-for (let n = 0 ; n < msg.length ; n++) {
+for (let n = 0; n < msg.length; n++) {
    let theta = Math.PI - 2 * Math.PI * n / msg.length;
    B[n] = [10 * Math.cos(theta) + 6, 5 * Math.sin(theta) - 1, 0];
    C[n] = B[n].slice();
