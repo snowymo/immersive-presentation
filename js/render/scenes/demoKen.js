3e639e5c1fed182123df2a69442edbfee767291b
Ken Perlin <ken.perlin@gmail.com>
2021-05-25 12:58:48 -0500

    Added window.vr flag to demoKen.js

--- js/render/scenes/demoKen.js
+++ js/render/scenes/demoKen.js
@@ -9,9 +9,9 @@ let DemoKen = function () {
      m.save();
         m.translate(0, 1.5 + 0.5 * Math.sin(3 * time), 0);
         if (Math.floor(time) % 10 < 5)
-           renderList.mCube().size(0.2).turnY(time).color([0, 1, 0]);
+           renderList.mCube().size(0.2).turnY(time).color(window.vr ? [1,1,0] : [0,1,0]);
         else
-           renderList.mCylinder().size(0.2).turnY(time).color([1, 0, 0]);
+           renderList.mCylinder().size(0.2).turnY(time).color([1,0,0]);
      m.restore();
   };
 };
