15bc5623e1ad80c650fb29c06bd148aa495afcc4
cdefanti <connor.defanti@gmail.com>
2021-06-15 10:36:56 -0700

    change realsense data from texture to array of points

--- js/render/scenes/demoRealSense.js
+++ js/render/scenes/demoRealSense.js
@@ -7,9 +7,9 @@ import "../../third-party/corelink.browser.lib.js";
 
 // expected dimensions of the incoming data texture
 // TODO: perhaps we could infer this from the first frame of data?
-const pixelWidth = 64;
-const pixelHeight = 48;
-let P = CG.particlesCreateMesh(pixelWidth * pixelHeight);
+const pixelWidth = 640;
+const pixelHeight = 480;
+let P = CG.particlesCreateMesh(10000);
 let dataPoints = [];
 
 let DemoRealSense = function () {
@@ -23,22 +23,32 @@ let DemoRealSense = function () {
       }
       if (dataPoints.length > 0) {
         // dimensions of the space the points occupy
-        let realWidth = 3.2;
-        let realHeight = 2.4;
+        const realWidth = 6.4;
+        const realHeight = 4.8;
         let R = [];
-        for (let j = 0; j < dataPoints.length; j++) {
-            let row = dataPoints[j];
-            for (let i = 0; i < row.length; i++) {
-                let rx = i * realWidth / pixelWidth;
-                let ry = realHeight - j * realHeight / pixelHeight;
-                let rz = row[i] / 1000;
-                let rrad = 0.1 / realWidth;
-                // TODO: color
-                let rr = rz;
-                let rg = 1;
-                let rb = 1;
-                R.push([rx, ry, rz, rrad, rr, rg, rb]);
-            }
+        for (let i = 0; i < dataPoints.length; i++) {
+          let idx = dataPoints[i][0];
+          let d = dataPoints[i][1];
+          let c = dataPoints[i][2];
+          let rx = (idx % pixelWidth) / pixelWidth * realWidth;
+          let ry = (1 - Math.floor(idx / pixelWidth) / pixelHeight) * realHeight;
+          let rz = d / 100;
+          let rrad = 0.007 * realWidth;
+          // TODO: color
+          let baseR = 0.3;
+          let baseG = 0.8;
+          let baseB = 1.2;
+          // faking scanlines
+          if (Math.abs(ry - time / 2) % 1.5 < 0.1) {
+            baseR = 1.0;
+            baseG = 1.2;
+            baseB = 1.6;
+          }
+          let rr = baseR * c / 256;
+          let rg = baseG * c / 256;
+          let rb = baseB * c / 256;
+
+          R.push([rx, ry, rz, rrad, rr, rg, rb]);
         }
         CG.particlesSetPositions(P, R, CG.matrixMultiply(viewMatrix[0], m.value()));
         m.save();
