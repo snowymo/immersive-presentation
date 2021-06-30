47c081ce370598318caa413ebf35eb2846f4694f
Ken Perlin <ken.perlin@gmail.com>
2021-06-30 08:33:08 -0500

    Added all remaining KEY_* in alphabet in input_keyboard.js. Tried setting a variable when Z key lifts in inline-viewer-helper.js. Showed that it worked by varying cube color in response in demoKen.

--- js/render/scenes/demoKen.js
+++ js/render/scenes/demoKen.js
@@ -8,10 +8,7 @@ let DemoKen = function () {
   this.display = () => {
      m.save();
         m.translate(0, 1.5 + 0.5 * Math.sin(3 * time), 0);
-        if (Math.floor(time) % 10 < 5)
-           renderList.mCube().size(0.2).turnY(time).color(window.vr ? [1,1,0] : [0,1,0]);
-        else
-           renderList.mCylinder().size(0.2).turnY(time).color([1,0,0]);
+        renderList.mCube().size(0.2).turnY(time).color(window.isSlideShow ? [1,1,0] : [0,1,0]);
      m.restore();
   };
 };
