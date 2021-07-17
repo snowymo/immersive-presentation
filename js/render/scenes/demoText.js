4269dd200ed4b8d70bd453bc3d9792f72c1d513d
rachel <rbo224@nyu.edu>
2021-07-16 20:15:03 -0400

    Draft of new TextDemo

--- js/render/scenes/demoText.js
+++ js/render/scenes/demoText.js
@@ -22,7 +22,7 @@ let DemoText = function () {
              C[n] = CG.mix(A[n], B[n], t);
           PT.layout(C, .5, .5);
 
-          m.rotateY(time);
+          m.rotateY(1);
           m.scale(.6);
 	  PT.render(renderList, m, [10,0,10]);
        m.restore();
@@ -31,14 +31,40 @@ let DemoText = function () {
   };
 };
 
-let msg = '  These are\nsome Metaroom\n   objects';
+let msg = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at lobortis nulla. Nunc sed neque sem. Sed quis quam tortor.';
 let PT = new CG.ParticlesText(msg);
 
 let A = PT.layout(null, .5,.5), B = [], C = [];
+let len = ((-1 - msg.length) / 3) * -1;
+let spaceCount = msg.length / 3;
+let lineNum = 0;
+//centered
+//console.log(msg.length);
 for (let n = 0 ; n < msg.length ; n++) {
-   let theta = Math.PI - 2 * Math.PI * n / msg.length;
-   B[n] = [10 * Math.cos(theta), 5 * Math.sin(theta), 0];
+   
+   if (msg[n] == " " && n >= spaceCount){
+      lineNum++;
+      spaceCount *= 2;
+      //B[n] = [n - (len*Math.floor(n/len)), 0 - Math.floor(n/len), 0];
+   }
+   console.log("n ",n);
+   console.log("lineNum ",lineNum);
+   console.log("spaceCount ", spaceCount);
+   console.log("mathfloor ",Math.floor(n/len));
+   console.log("len", len);
+   console.log("len* mathfloor ", len*Math.floor(n/len));
+   console.log(" n - len*mathfloor ",n - (len*Math.floor(n/len)));
+   console.log("n - len*linenum ", n - (len*lineNum));
+   //B[n] = [n - (len*Math.floor(n/len)), 0 - Math.floor(n/len), 0];
+   B[n] = [n - (len*lineNum), lineNum, 0];
    C[n] = B[n].slice();
+   console.log("arr ", C[n])
+   
 }
 
+/*
+TO DO:
+right align, left align
+word based vs character baseds
+*/
 export let demoText = new DemoText();
