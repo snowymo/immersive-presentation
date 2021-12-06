a0a60f801331573bb28faa13c7e0f0ba6053a82f
rachel <rbo224@nyu.edu>
2021-12-06 14:02:08 -0500

    final version

--- js/render/scenes/demoText.js
+++ js/render/scenes/demoText.js
@@ -7,6 +7,10 @@ import {
    controllerMatrix,
    buttonState,
  } from "../core/renderListScene.js";
+
+let xPos = 0;
+let yPos = 1.5;
+let zPos = 1;
 let DemoText = function () {
   this.background = null;
   this.loadGLTF = false;
@@ -23,26 +27,49 @@ let DemoText = function () {
 
        m.save();
           let t = Math.max(0, Math.min(1, .5 + Math.sin(time)));
-
-
           
           m.scale(.6);
+          m.translate(xPos, yPos, 0);
 	  PT.render(renderList, m, [10,0,10]);
        m.restore();
 
     m.restore();
+    
   };
 };
 
 let msg = '';
 let PT = new CG.ParticlesText(msg);
 let A = PT.layout(null, .5,.5), B = [], C = [];
+document.addEventListener("mousemove", (event) =>{
+   if (event.buttons & 1) {
+      xPos += event.movementX / 5;
+      yPos += event.movementY / 5;
+      zPos = window.avatars[window.playerid].headset.position.z + 1;
+    }
+});
 
 document.addEventListener("keydown", (event) => {
-   if ((32 <= event.keyCode <= 97) && (keyboardInput.keyIsUp(event.keyCode))) {
+ 
+   if (event.keyCode == 9){
+      msg += '    ';
+   }
+   else if (event.keyCode == 13){
+      msg += '\n';
+   }
+   else if (event.keyCode == 32){
+      msg += ' ';
+   }
+   else if (event.keyCode == 8){
+      msg.slice(0, -1);
+   }
+   else if ((event.keyCode == 91) || (event.keyCode == 16) || (event.keyCode == 27)){
+      msg += '';
+   }
+  else if ((48 <= event.keyCode <= 90) && (keyboardInput.keyIsUp(event.keyCode))) {
      msg += event.key;
    }
-    console.log(msg);
+   
     PT = new CG.ParticlesText(msg);
  });
 
@@ -50,7 +77,7 @@ for (let n = 0 ; n < msg.length ; n++) {
    let theta = Math.PI - 2 * Math.PI * n / msg.length;
    B[n] = [10 * Math.cos(theta), 5 * Math.sin(theta), 0];
    C[n] = B[n].slice();
-
+ 
 }
 
 export let demoText = new DemoText();
